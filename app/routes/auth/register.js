905d13e7b4861fa0ef9acf9512d53e8103354504
feipinghuang <feiping.huang@thexstore.cn>
2016-07-08 10:18:15 +0800

    add auth work

---
+++ app/routes/auth/register.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    doRegister() {
+      this.get('currentModel').save()
+         .then(() => {
+           this.transitionTo('auth.login');
+         });
+    }
+  },
+  model() {
+    return this.store.createRecord('user');
+  }
+});
